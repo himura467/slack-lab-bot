4a30b65538e4bc8f73913b775b5539a63f066612
himura467 <akitoshitara@gmail.com>
2026-05-15 09:51:05 +0900

    feat: `conversations.history` method response model

---
+++ src/infrastructure/slack/model.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+pub struct Message {
+    pub subtype: Option<String>,
+    pub user: Option<String>,
+}
+
+#[derive(Deserialize)]
+pub struct ResponseMetadata {
+    pub next_cursor: Option<String>,
+}
+
+#[derive(Deserialize)]
+pub struct ConversationsHistoryResponse {
+    pub ok: bool,
+    pub messages: Option<Vec<Message>>,
+    pub response_metadata: Option<ResponseMetadata>,
+    pub error: Option<String>,
+}
