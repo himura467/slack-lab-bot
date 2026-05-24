6071c0c612c8c50903cad128bc87a780feeb46c3
himura467 <akitoshitara@gmail.com>
2026-05-24 20:30:48 +0900

    refactor(domain): rename port methods to reflect intent

--- src/domain/port.rs
+++ src/domain/port.rs
@@ -3,10 +3,10 @@ use super::model::Message;
 pub trait SlackClient {
     type Error: std::error::Error + Send + Sync + 'static;
 
-    async fn fetch_messages(
+    async fn fetch_history(
         &self,
         channel_id: &str,
         oldest_ts: i64,
     ) -> Result<Vec<Message>, Self::Error>;
-    async fn post_message(&self, channel_id: &str, text: &str) -> Result<(), Self::Error>;
+    async fn send_message(&self, channel_id: &str, text: &str) -> Result<(), Self::Error>;
 }
