7b5dd5df16ee7377132bf8a494e0c668c8aa69ee
himura467 <akitoshitara@gmail.com>
2026-05-26 00:50:55 +0900

    feat(adapter): Lambda primary adapter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+mod adapter;
+mod domain;
+mod service;
+
+#[tokio::main]
+async fn main() -> Result<(), lambda_runtime::Error> {
+    adapter::primary::lambda::run().await
 }
